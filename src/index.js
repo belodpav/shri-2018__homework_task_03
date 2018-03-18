ac018bf722739494e0342a11c2140f99bca23b30
belodpav <belod.pav@gmail.com>
2018-03-18 03:36:27 +0300

    index.js - Точка входа в приложение

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+import VideoStream from './videoStream';
+import AudioStream from './audioStream';
+import TermInterface from './termInterface';
+import updateText from './animatedText/animatedText';
+import css from './index.css';
+
+//  Число частотных полос гистограммы
+const AF_COUNT = 16;
+
+const appInterface = new TermInterface('.app__i', {itemsCount: AF_COUNT});
+const audio = new AudioStream(AF_COUNT);
+
+const video = new VideoStream('.app', audio, appInterface);
+
+// Анимируем текст в итерфейсе
+updateText();
